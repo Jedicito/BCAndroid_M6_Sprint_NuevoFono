08087fcfc6067573442fc91bb7ca5081721fa46d
Jedicito <seron.cesar@gmail.com>
2023-08-15 18:14:28 -0400

    Commit inicial. Gradle listo.

---
+++ app/src/main/java/chl/ancud/m6_sprint_nuevofono/MainActivity.kt
@@ -0,0 +1,11 @@
+package chl.ancud.m6_sprint_nuevofono
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
